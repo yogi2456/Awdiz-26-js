176002513849b54079642558a0852f947ddfd76a
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2024-02-08 17:55:02 +0530

    updates

---
+++ day-2/index.js
@@ -0,0 +1,36 @@
+{
+    let myName = "awdiz";
+    console.log(myName)
+}
+//console.log(myName)
+
+{
+    const mySurname = "abc";
+    console.log(mySurname)
+}
+//console.log(mySurname)
+
+{
+    var myAge = 24;
+    console.log(myAge)
+}
+console.log(myAge)
+
+// Comparision operator
+{/* < >  <= >= == === */}
+console.log(2 < 1)
+console.log(2 < 3)
+console.log(3 > 4)
+console.log(3 > 2)
+console.log(4 <= 4)
+console.log(4 <= 5)
+console.log(4 <= 3)
+console.log(2 >= 2)
+console.log(2 >= 1)
+console.log(2 >= 3)
+console.log(2 == 2)
+console.log(2 == 3)
+console.log(2 == "2")   // == : it checks only data
+console.log(2 === 4)
+console.log(2 === "2")
+console.log("2" === "2")     // === : it checks data and datatypes

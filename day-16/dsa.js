8ab6983a85a6a9e318c366c833a7f789edc83332
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2024-03-20 19:34:12 +0530

    updates

--- day-16/dsa.js
+++ day-16/dsa.js
@@ -72,15 +72,16 @@ const findMaxSumPair = function (numbers) {
 console.log(findMaxSumPair(numbers));
 
 
-// const names = ["Alice", "Bob", "Anna", "Alex", "Charlie", "Amanda"];
+const names = ["Alice", "Bob", "Anna", "Alex", "Charlie", "Amanda"];
 
-// const countNames = function (names) {
-// let count=0;
-// for(i=0;i<names.length;i++){
-//     if(names[i][0]==="A"){
-//         count++;
-//     }
-// }
-// };
+const countNames = function (names) {
+let count=0;
+for(i=0;i<names.length;i++){
+    if(names.toUpperCase()[0] === "A"){
+        count++;
+    }
+}
+    return count;
+};
 
-// console.log(countNames(names));
+console.log(countNames(names));

89d0348088de002330e96a38f777bdda5aa956fc
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2024-03-22 14:57:09 +0530

    updates

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-// var array = [1, 2, 4, 56, 7, 8, 3, 2, 5, 7];
-// var target = 12
+var array = [2, 7, 11, 15];
+var target = 9
+var result = []
 
-// function FindTargetedValues(array, target) {
-//     for (var i = 0; i < array.length - 1; i++) {
-//         for (var j = i + 1; j < array.length; j++) {
-//             if(array[i] + array[j] == target) {
-//                 console.log(array[i], array[j])
-//             }
-//             // console.log(array[i], array[j])
-//         }
-//     }
-// }
+function FindTargetedValues(array, target) {
+    for (var i = 0; i < array.length - 1; i++) {
+        for (var j = i + 1; j < array.length; j++) {
+            if(array[i] + array[j] == target) {
+                //result.push(i, j)
+                console.log(i,j)
+            }
+             //console.log(array[i], array[j])
+        }
+    }
+    return result;
+}
 
 
-// FindTargetedValues(array, target)
+FindTargetedValues(array, target)
 
 // Q. Create a function that takes a string and returns a count of unique words. (, . are not words)
 
@@ -21,19 +24,19 @@
 
 // Output :  { Hi : 1, My : 1, name : 1, is : 1 …   }
 
-var string = "Hi, My name is Awdiz, I am located at Vashi."
-count = 0;
+// var string = "Hi, My name is Awdiz, I am located at Vashi."
+// count = 0;
 
-function FindString(string) {
-    // for (var i = 0; i < string.length; i++) {
-    //     // console.log(string[i])
-    //     // if(string[i] == string[i]) {
-    //     //     count += i;
-    //     // }
-    //     // return string.split(' ').length;
-    // }
-    // // return count;
-    return string.split(' ').length;
-}
+// function FindString(string) {
+//     // for (var i = 0; i < string.length; i++) {
+//     //     // console.log(string[i])
+//     //     // if(string[i] == string[i]) {
+//     //     //     count += i;
+//     //     // }
+//     //     // return string.split(' ').length;
+//     // }
+//     // // return count;
+//     return string.split(' ').length;
+// }
 
-FindString(string)
+// FindString(string)
